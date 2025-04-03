e590213ee9219b90838e1b0055cf73e79a6a76a2
Reidruss <Reidrussell217@gmail.com>
2025-04-02 21:22:14 -0400

    testing

--- obj/Debug/net9.0/AssemblerProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/AssemblerProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssemblerProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab344448592659718a11efa63d02cf6f25bff3f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a834bb22e2fe9d88ad92a4a12d784cfdaa1643be")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssemblerProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssemblerProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
